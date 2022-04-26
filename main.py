006412ec26a7a6208f7b2787696de429897475aa
laurebt <laurebt@users.noreply.github.com>
2022-04-26 11:16:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
+# https://huggingface.co/microsoft/swin-tiny-patch4-window7-224
+
 from transformers import AutoFeatureExtractor, SwinForImageClassification
 from PIL import Image
 import requests
+import streamlit as st
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+#######################################################
+def __get_image__():
+  
+  st.subgeader("Image")
+  
+  image_file = st.file_uploader(label="Upload an image", type=["jpg", "jpeg"])
+  
+  if image_file is not None:
+    
+    # To See details
+    file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
+		st.write(file_details)
 
-feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
-model = SwinForImageClassification.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
+    # To View Uploaded Image
+		image = Image.open(image_file)
+    
+   return image
+  
+#######################################################
+# Image classification
+def compute(image):
 
-inputs = feature_extractor(images=image, return_tensors="pt")
-outputs = model(**inputs)
-logits = outputs.logits
-# model predicts one of the 1000 ImageNet classes
-predicted_class_idx = logits.argmax(-1).item()
-print("Predicted class:", model.config.id2label[predicted_class_idx])
+  feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
+  model = SwinForImageClassification.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
+
+  inputs = feature_extractor(images=image, return_tensors="pt")
+  outputs = model(**inputs)
+  logits = outputs.logits
+  
+  # model predicts one of the 1000 ImageNet classes
+  predicted_class_idx = logits.argmax(-1).item()
+  print("Predicted class:", model.config.id2label[predicted_class_idx])
+
+#######################################################
+if __name__ == '__main__':
+  
+  st.title("Sentiment analysis on Breaking Bad quotes")
+  image = __get_image__()
+  compute(image):
+    
+    
