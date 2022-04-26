3c52e9b9f205b57f945cb5a544277580fb4c94e9
laurebt <laure@belmont.tech>
2022-04-26 11:33:55 -0600

    main

--- main.py
+++ main.py
@@ -6,43 +6,43 @@ import requests
 import streamlit as st
 
 #######################################################
-def __get_image__():
-  
-  st.subgeader("Image")
-  
-  image_file = st.file_uploader(label="Upload an image", type=["jpg", "jpeg"])
-  
-  if image_file is not None:
-    
-    # To See details
-    file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
-		st.write(file_details)
+def __get_image__(image_file):
 
-    # To View Uploaded Image
-		image = Image.open(image_file)
-    
-   return image
-  
+	file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
+	img = Image.open(image_file)
+
+	return img, file_details
+
+
 #######################################################
 # Image classification
-def compute(image):
+def compute(img):
 
-  feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
-  model = SwinForImageClassification.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
+	feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
+	model = SwinForImageClassification.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
 
-  inputs = feature_extractor(images=image, return_tensors="pt")
-  outputs = model(**inputs)
-  logits = outputs.logits
-  
-  # model predicts one of the 1000 ImageNet classes
-  predicted_class_idx = logits.argmax(-1).item()
-  print("Predicted class:", model.config.id2label[predicted_class_idx])
+	inputs = feature_extractor(images=img, return_tensors="pt")
+	outputs = model(**inputs)
+	logits = outputs.logits
+
+	# model predicts one of the 1000 ImageNet classes
+	predicted_class_idx = logits.argmax(-1).item()
+
+	return model.config.id2label[predicted_class_idx]
 
 #######################################################
 if __name__ == '__main__':
-  
-  st.title("Sentiment analysis on Breaking Bad quotes")
-  image = __get_image__()
-  compute(image):
-    
-    
+
+	st.title("Image classification")
+
+	st.subheader("Image")
+	image_file = st.file_uploader(label="Upload an image", type=['jpg', 'jpeg'])
+
+	if image_file is not None:
+		img, file_details = __get_image__(image_file)
+		# st.write(file_details)
+		st.image(img,caption='uploaded image')
+
+		st.subheader("IPrediction")
+		prediction = compute(img)
+		st.text(prediction)
